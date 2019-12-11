25c68106b0a9437d71ceb9cd25c6b6148b3bd959
Darin PC Git <infinitedopeness@gmail.com>
2019-12-10 16:11:55 -0800

    Updated /react

--- practice/react/myfirstapp/src/index.js
+++ practice/react/myfirstapp/src/index.js
@@ -28,14 +28,60 @@ const paragraph = React.createElement(
     "This is a paragraph"
 );
 
+// some HTML may be named the same
+// function component needs to be capitalized...
+// always must return JSX
+function About() {
+  return (
+    <p>This is officially a react function component</p>
+  );
+}
+
+// props == "react properties"
+
+function H1Generator (props) {
+  return (
+    <h1>{props.text}</h1>
+  );
+}
+
+const H2Generator = (props) => {
+  return (<h2>{props.something}</h2>); 
+}
+
+// class components are better than function components:: car : bicycle
+// current class inherits properties and methods from parent class
+class Counter extends React.Component {
+  constructor() {
+    super();
+// should not directly access this object, tell the object to chage itself, for security purposes
+// review special way to update states for class components
+    this.state = {
+      score: 0
+    };
+  }
+
+  // cannot directly access {this.state}
+render(props) {
+  return (<p>The current score is: {this.props.tempScore}</p>);
+}
+
+}
+
 const idOfDiv = "test";
 
 // in b/w parens is JSX
 
+
+// ANY JS inside of curly braces
 // JSX version of createElement 
 const app = (
     // to interpolate JS use {}
     <div id={idOfDiv}>
+      <About tempScore={3} />
+      <H1Generator text="Hi this custom text!" />
+      <H1Generator text="This is some other line!" />
+      <H2Generator something="Some text" />
       <h1>{someTitle}</h1>  
       <p>{title}</p>
       {paragraph}
@@ -50,8 +96,6 @@ const footer2 = React.createElement(
     {id: "footer"}
 );
 
-
-
 // render(): "please put this on the webpage"
 // render will take one component or composition
 // Render componenet to target element
@@ -59,3 +103,13 @@ ReactDOM.render(
     app,
     document.getElementById("root")
 );
+
+// JSX syntax
+// anything written in JSX should go in b/w a pair of parens
+// Any JS inside of JSX should be written b/w a pair of {}
+// EXAMPLE:
+// let text = (
+//   <p>{SomeJSVariable}</p>
+// );
+
+// Angular uses typescript...
